88f732b8997f15d42074522827ea24ae247a81ee
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-02-05 00:50:20 +0100

    Delete mainPageLangRedirect.js

--- javascript/mainPageLangRedirect.js
+++
@@ -1,7 +0,0 @@
-var lang = navigator.language || navigator.browserLanguage; //for IE
-        
-if (lang.includes("pt")) {
-    window.location.replace("/pt");
-} else {
-    window.location.replace("/en");
-}
