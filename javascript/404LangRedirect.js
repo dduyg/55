ca78d70864eb72e05a193807cafc75ea4432661b
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-02-05 01:02:55 +0100

    Delete 404LangRedirect.js

--- javascript/404LangRedirect.js
+++
@@ -1,7 +0,0 @@
-var lang = navigator.language || navigator.browserLanguage; //for IE
-
-if (lang.includes("pt")) {
-    window.location.replace("/pt/404");
-} else {
-    window.location.replace("/en/404");
-}
