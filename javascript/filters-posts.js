c61dc63ed0dc1b0e607f2daf8cdb07e369842956
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-03-17 19:27:55 +0100

    Update filters-posts.js

--- javascript/filters-posts.js
+++ javascript/filters-posts.js
@@ -1,6 +1,6 @@
 let selector_query = window.location.hash.substr(1);
-let filterButtons = $('.item.filter')
-let filterDropdown = $('.labeled.icon.dropdown.button')
+let filterButtons = $('.item.filter');
+let dropdown = $('.ui.dropdown');
 
 if (selector_query) {
     let element = $('#filter-' + selector_query)
@@ -10,7 +10,6 @@ if (selector_query) {
         filterButtons.removeClass('active');
         element.addClass('active');
         element.blur();
-        filterDropdown.dropdown('set selected', selector_query);
     }
 }
 
@@ -20,15 +19,13 @@ filterButtons.click(function () {
     filterButtons.removeClass('active');
     $(this).addClass('active');
     $(this).blur();
-    filterDropdown.dropdown('set selected', this.id.replace("filter-", ""));
-})
+});
 
 $('#filter-all').click(function() {
     filterButtons.removeClass('active');
-    $(this).addClass('active');
-    $(this).blur();
-    filterDropdown.dropdown('clear');
-})
+    dropdown.dropdown('restore defaults');
+    filter('all');
+});
 
 function filter(category) {
     let selector = $('#filterable > div, #filterable > a')
